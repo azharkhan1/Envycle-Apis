76c5a3c5db52d7111aebda10c6acbe9c8908d292
azhar khan <azhar40@live.co.uk>
2021-02-16 23:27:49 +0500

    credtieals true for the server testing

--- server/server.js
+++ server/server.js
@@ -27,21 +27,21 @@ var server = http.createServer(app);
 var io = socketIo(server);
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+
 app.use(cors({
     origin: "http://localhost:3000",
     credentials: true,
-    sameSite : "none",
 }));
+
 app.use(cookieParser());
 
 
 app.use((req,res,next)=>{
-
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control, Authorisation");
     next();
-
 })
 
 
