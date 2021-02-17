002cb84dde22c7c1cfb4a9591274aa7bb12cdce9
azhar khan <azhar40@live.co.uk>
2021-02-17 19:29:43 +0500

    checking

--- server/server.js
+++ server/server.js
@@ -37,10 +37,10 @@ app.use(cookieParser());
 
 
 app.use((req,res,next)=>{
-    // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Credentials", true);
-    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control, Authorisation");
+    header("Access-Control-Allow-Origin: http://localhost:3000");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, POST");
+    header("Access-Control-Allow-Headers: Content-Type, *");
     next();
 })
 
